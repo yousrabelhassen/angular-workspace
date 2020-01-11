c5946486819e32f3b14748f2b9adb94817bb07b6
benbelhassen.yousra@gmail.com <benbelhassen.yousra@gmail.com>
2020-01-11 15:46:07 +0100

    Initial commit

---
+++ projects/toaster/src/lib/toaster.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-toaster',
+  template: `
+    <p>
+      toaster works!
+    </p>
+  `,
+  styles: []
+})
+export class ToasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
